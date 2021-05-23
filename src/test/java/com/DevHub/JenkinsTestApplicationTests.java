4c7e127ad53cc908d4d736339c3d73d964e59280
akshaydevsv21 <akshaydevsv21@gmail.com>
2021-05-23 17:48:58 +0530

    First commit

---
+++ src/test/java/com/DevHub/JenkinsTestApplicationTests.java
@@ -0,0 +1,20 @@
+package com.DevHub;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTestApplicationTests {
+
+	public static Logger logger=LoggerFactory.getLogger(JenkinsTestApplicationTests.class);
+	@Test
+	void contextLoads() {
+		logger.info("Application Tests are Executing");
+		assertEquals(true, true);
+	}
+
+}
